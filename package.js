d4e1774570357baeae0afe4c0409a9a2897beeb8
Adel Qalieh <aqalieh95@gmail.com>
2016-06-16 14:52:12 -0400

    Update to latest versions of moment and moment-recur

--- package.js
+++ package.js
@@ -1,11 +1,11 @@
 Package.describe({
   summary: "A momentjs plugin for matching and generating recurring dates.",
-  version: "1.0.0",
+  version: "1.0.5",
   git: "https://github.com/adelq/meteor-moment-recur.git"
 });
 
 Package.onUse(function(api) {
   api.versionsFrom('METEOR@0.9.3.1');
-  api.use('mrt:moment@2.8.1');
+  api.use('momentjs:moment@2.13.0');
   api.addFiles('lib/moment-recur/moment-recur.js');
 });
